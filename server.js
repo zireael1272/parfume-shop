c7dc3140b1831fe658076353be08eb819fd97bfc
zireael1272 <kseniagonsharenko2007@gmail.com>
2025-11-29 12:31:18 +0200

    Reorganization

--- server.js
+++ server.js
@@ -2,16 +2,22 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import Router from "./routes.js";
+import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const Mongo = process.env.MONGO_URL;
 
 const app = express();
+const __dirname = path.resolve();
 
-app.use(express.static("public"));
+app.use(express.static(__dirname));
 app.use(express.json());
 app.use("/api", Router);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 mongoose
   .connect(Mongo)
@@ -19,10 +25,6 @@ mongoose
     console.log("DB is connection");
   })
   .catch((error) => console.error("DB connection error:", error));
-
-app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: "public" });
-});
 
 app.listen(PORT, () => {
   console.log("listing on port 3000");
